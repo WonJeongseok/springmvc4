c973940b09cb9763080d40ff2a0e20d279564696
hduser <hduser@pebriq-server>
2017-11-21 17:09:53 +0900

    17/11/21

--- springmvc4/src/main/java/com/newlecture/web/config/WebApplicationContextConfig.java
+++ springmvc4/src/main/java/com/newlecture/web/config/WebApplicationContextConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration		//	"구성을 담당하는 애입니다" 라고 알려준다
 @EnableWebMvc		//	
 @Component("com.newlecture.web.config")	//	패키지의 경로
+
 public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {		// 기존에 쓰던 ServletContext.xml과 같다
 	
 
